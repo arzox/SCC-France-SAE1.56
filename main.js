13c927568834cf7c39c124f2df6e1f806f777513
Sylent <aimery.pellerin@gmail.com>
2024-01-23 22:41:09 +0100

    Title

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elements = document.querySelectorAll('.to-load');
     var i = 0;
-    sleep(200)
     while (i < elements.length) {
         var element = elements[i];
         element.classList.add('loaded');
         i++;
     }
 });
-
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-        if ((new Date().getTime() - start) > milliseconds){
-            break;
-        }
-    }
-}
